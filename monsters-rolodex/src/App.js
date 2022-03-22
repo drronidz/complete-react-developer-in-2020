5a7c64c019aed2784ac60e7bc104719e1d028a69
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-23 00:13:16 +0100

    Updating dependencies & Understanding class Components ...

---
+++ monsters-rolodex/src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            name: 'ABD EL HALIM'
+        }
+    }
+
+  render() {
+    return (
+        <div>
+            <h1>Hello My name is {this.state.name}</h1>
+            <button onClick={() => this.setState({name: 'Hello John'})}>Click</button>
+        </div>
+    );
+  }
+
+}
+
+export default App;
