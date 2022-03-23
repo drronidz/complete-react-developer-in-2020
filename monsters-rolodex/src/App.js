96afa44b86baeafec8914beea7aa7148d00ac605
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-23 13:37:34 +0100

    Finalising the App ...

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {CardList} from "./components/card-list/card-list.component";
 import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
+import {Title} from "./components/title/title.component";
 
 class App extends Component {
 
@@ -18,9 +19,10 @@ class App extends Component {
     componentDidMount() {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
+            .then(users => console.log(this.setState({monsters: users})))
     }
 
-    handleChange = (event) => { this.setState({searchField: event.target.value}) }
+    onSearchChange = (event) => { this.setState({searchField: event.target.value}) }
 
     render() {
         const { monsters, searchField } = this.state
@@ -28,8 +30,9 @@ class App extends Component {
         console.log(filteredMonsters)
         return (
             <div className='App'>
+                <Title title={" Monster Rolodex "}/>
                 <SearchBox
-                    handleChange={this.handleChange}
+                    handleChange={this.onSearchChange}
                     placeholder="search monsters"/>
                 <CardList monsters={filteredMonsters}/>
             </div>
---
+++ monsters-rolodex/src/components/title/title.component.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import './title.styles.css'
+
+export const Title = ({title}) => (
+    <h1 className="title">{title}</h1>
+)
+
