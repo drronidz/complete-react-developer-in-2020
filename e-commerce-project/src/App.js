da212858507cda978df60b5f17b4353b58552502
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-25 00:06:10 +0100

    Setting Firebase authentication Part 2 ...

--- e-commerce-project/src/App.js
+++ e-commerce-project/src/App.js
@@ -6,18 +6,45 @@ import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
+//Firebase dependencies
+import { auth } from "./firebase/firebase.utilis";
 
-function App() {
-  return (
-    <div>
-        <Header/>
-        <Switch>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/shop" component={ShopPage}/>
-            <Route path="/sign-in" component={SignInAndSignUpPage}/>
-        </Switch>
-    </div>
-  );
+
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser : null
+        }
+    }
+
+    unsubscribeFromAuth = null
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged( user => {
+            this.setState({ currentUser : user})
+
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth()
+    }
+
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Switch>
+                    <Route exact path="/" component={HomePage}/>
+                    <Route path="/shop" component={ShopPage}/>
+                    <Route path="/sign-in" component={SignInAndSignUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
