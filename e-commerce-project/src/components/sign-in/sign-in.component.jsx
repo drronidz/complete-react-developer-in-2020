6d13bb86d1fe60222d745b89a01154881b9fce17
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-25 00:21:52 +0100

    Setting Firebase authentication Part 3 ...

--- e-commerce-project/src/components/sign-in/sign-in.component.jsx
+++ e-commerce-project/src/components/sign-in/sign-in.component.jsx
@@ -54,10 +54,11 @@ class SignIn extends Component{
                                label="password"
                                required
                     />
-                    <CustomButton type="submit">Sign in</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>{' '}Sign in with Google{' '}</CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign in</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>{' '}Sign in with Google{' '}</CustomButton>
+                    </div>
                 </form>
-
             </div>
         )
     }
