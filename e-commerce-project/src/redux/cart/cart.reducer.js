e49f86ee7317c46dcf579b65dd916be26bace301
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-26 00:58:06 +0100

    Setting AddToCart Part Two ...

--- e-commerce-project/src/redux/cart/cart.reducer.js
+++ e-commerce-project/src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import CartActionTypes from "./cart.types";
+import {addItemToCart} from "../../components/cart/utilis/cart.utils";
 
 
 const INITIAL_STATE = {
@@ -18,7 +19,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         case CartActionTypes.ADD_ITEM:
             return {
                 ...state,
-                cartItems: [...state.cartItems, action.payload]
+                cartItems: addItemToCart(state.cartItems, action.payload)
             }
 
         default:
