ef4d65c8edc2e0c15dce397fdd329fac87054777
DRRONIDZ <abdelhalim.diab@univ-constantine2.dz>
2022-03-29 00:29:05 +0200

    Adding Selector to the CartItemsCount ...

--- e-commerce-project/src/components/cart/cart-icon/cart-icon.component.jsx
+++ e-commerce-project/src/components/cart/cart-icon/cart-icon.component.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-
-
 import { ReactComponent as ShoppingIcon } from '../../../assets/shopping-bag.svg';
+import {selectCartItemsCount} from "../../../redux/cart/cart.selectors";
 
 import './cart-icon.styles.scss';
 import {toggleCartHidden} from "../../../redux/cart/cart.actions";
@@ -15,10 +14,8 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
     </div>
 );
 
-const mapStateToProps = ({ cart : {cartItems}}) => ({
-    itemCount: cartItems.reduce((accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,0
-    )
+const mapStateToProps = state => ({
+    itemCount: selectCartItemsCount(state)
 })
 
 const mapDispatchToProps = dispatch => ({
